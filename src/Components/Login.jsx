f074179a780348beb09c07d2501738fc877b56eb
Vickrant <vickrantselvendran@gmail.com>
2025-03-02 21:23:33 +0530

    Login/Logout system added

---
+++ src/Components/Login.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../App.css';
+
+export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const navigate = useNavigate();
+
+    const C_username = "Kasi Selvendran";
+    const C_password = "selva@1972";
+
+    localStorage.setItem('username', C_username);
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        if (username === C_username && password === C_password) {
+            localStorage.setItem("isAuthenticated", "true");
+            localStorage.setItem("loginTime", Date.now())
+            navigate("/dashboard");
+        } else {
+            alert("Invalid Credentials");
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <form onSubmit={handleLogin}>
+                <label>
+                    <p>Username</p>
+                    <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input className='inputPass' type={showPassword ? "text" : "password"} placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <div className="show-password">
+                        <input type='checkbox' onChange={() => setShowPassword(!showPassword)} />
+                        <span>Show password</span>
+                    </div>
+                </label>
+                <button type='submit'>Login</button>
+            </form>
+        </div>
+    );
+}
