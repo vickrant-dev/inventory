6165eb9bf571c19a0fe7e683c8ae8f40e8ecd6c4
Vickrant <vickrantselvendran@gmail.com>
2025-03-15 23:05:30 +0530

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Dashboard from './Components/Dashboard';
 
 export default function App() {
     return (
-        <Router>
+        <Router basename='/inventory'>
             <Routes>
                 <Route path='/login' element={<Login />} />
                 <Route path='/dashboard' element={<Dashboard />} />
